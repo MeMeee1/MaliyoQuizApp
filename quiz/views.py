b75152429e12f15c16c1627ed0db81fceba81189
Ysabel Katchy <ykatchy@gmail.com>
2023-01-23 16:50:34 +0100

    Updates

--- quiz/views.py
+++ quiz/views.py
@@ -98,10 +98,9 @@ class ResultView(View):
         score = 0
         for question in questions:
             correct_option = question.get_correct_option()
-            user_answer = request.POST.get(question.id)
-            
+            user_answer = request.POST.get(str(question.id))
             if user_answer == correct_option.value:
-                score += form.get_total_score()
+                score += question.score_amount
         return render(request, 'form_results.html', {'score': score})
 def bootstrap4_index(request):
     return render(request, "form_thanks.html", {})
